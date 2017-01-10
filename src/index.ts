d51e934aee778c1774f7c6ba321c6f0ce5335a4c
yortus <yortus@gmail.com>
2017-01-10 18:41:41 +0800

    find stems initial work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
+import * as path from 'path';
+import * as glob from 'glob';
+
+
+
+
+
+// TODO: temp testing...
 console.log('Hello, World!');
+
+
+// Find all modules recursively, using the presence of package.json as a proxy to identify them
+let modulePaths = glob.sync('node_modules/**/package.json').map(fn => fn.slice(0, -12));
+console.log('=================================');
+console.log(modulePaths);
+
+
+// Work out which of these are stems. ie can resolve 'module/stem'.
+let stemPaths = modulePaths.map(mp => {
+    try {
+        return require.resolve(path.join(mp, 'stem'));
+    }
+    catch (ex) {
+        return null;
+    }
+});
+console.log('=================================');
+console.log(stemPaths);
+
