ff0401ddf400d8f1b1db9c398e0c879e0334e5e5
yortus <yortus@gmail.com>
2017-01-24 14:04:51 +0800

    export StemMain and StemInfo types

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,5 @@
 export {default as build} from './build';
 export {default as list} from './list';
 export {default as start} from './start';
+export {default as StemInfo} from './stem-info';
+export {default as StemMain} from './stem-main';
